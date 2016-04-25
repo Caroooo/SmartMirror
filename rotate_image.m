6998984d18dc4621eaf6cba9399a699db71288ed
Kolja Dobrovoljski <kdi15001@student.mdh.com>
2016-04-25 18:48:16 +0200

    deleted all unneccesary files

--- rotate_image.m
+++
@@ -1,7 +0,0 @@
-function rotim = rotate_image(image, orientation)
-rotim = imrotate(image,orientation/pi*180 - 90,'nearest', 'crop');
- figure,imshow(rotim);
-cropsze = fix(size(image,1)/sqrt(2));
-offset = fix((size(image,1)-cropsze)/2);
-rotim = rotim(offset:offset+cropsze, offset:offset+cropsze);
-  figure,imshow(rotim);
